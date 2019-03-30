28434b1ec7f8f10d4f07c8be7bc90c4d41f4fb56
Mike Oh <michaelohw5@gmail.com>
2019-03-30 16:23:41 -0700

    Updates

---
+++ precache-manifest.332672a0cb657a4f0fea21634880f76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickgame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  },
+  {
+    "revision": "6cf06fee2670decb7439",
+    "url": "/clickgame/static/js/main.82c7f6e6.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickgame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6cf06fee2670decb7439",
+    "url": "/clickgame/static/css/main.119fea83.chunk.css"
+  },
+  {
+    "revision": "13c01e291d51acfd204618ab0ac26e3f",
+    "url": "/clickgame/index.html"
+  }
+];
