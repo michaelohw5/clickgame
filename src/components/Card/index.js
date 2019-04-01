b0468ee23df36f8672f6122a2370de016a6bf963
Mike Oh <michaelohw5@gmail.com>
2019-03-31 20:26:16 -0700

    added more characters

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Card(props) {
     return (
-        <div className="card col-md-4">
+        <div className="card col-md-3">
             <div className="img-container" onClick={() => props.checkAndShuffle(props.id)}>
                 <img alt={props.name} src={props.image}/>
             </div>
